a8b685ace3732543c05c2ed5aff5503aed380296
Herman <58345489+FUFAFUFA@users.noreply.github.com>
2024-01-17 19:31:09 +0100

    JS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // const (constant, never changes)
+
 // let (let me change)
 
 // WEB-API
-const scoreCounter = document.querySelector('score-counter');
-console.log(Nathan);
+const scoreCounter = document.querySelector('core-counter');
+const grid = document.querySelector('.grid');
