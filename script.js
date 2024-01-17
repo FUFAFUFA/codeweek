921f06b9c4d1463dd356f41598d8a4944929375f
Herman <58345489+FUFAFUFA@users.noreply.github.com>
2024-01-17 21:31:47 +0100

    js

--- script.js
+++ script.js
@@ -11,19 +11,45 @@ const playAgainButton = document.querySelector('.play-again');
 const totalCells = 100;
 const totalBombs = 90;
 const maxScore = 5;
-const bomsList = [];
+const bombsList = [];
 
 let score = 0;
+
+function updateScore() {
+    score++;
+    scoreCounter.innerText = score.toString().padStart(5, '0');
+
+    if (score === maxScore){
+        endGame();
+    }
+}
 
 const cell = document.createElement('div');
 cell.classList.add('cell');
 
 for (let i = 1; i <= 100; i++) {
     const cell = document.createElement('div');
-    cell.classList.add(cell);
+    cell.classList.add('cell');
 
     cell.addEventListener('click', function (){
-        console.log()
-    })
+        cell.classList.add('cell-clicked');
+        updateScore();
+    });
+
     grid.appendChild(cell);
+
+}
+
+while (bombsList.length < totalBombs) {
+    const randomNumber = Math.floor(Math.random() * totalCells) + 1;
+
+    if (!bombsList.includes(randomNumber)) {
+        bombsList.push(randomNumber);
+    }
+}
+
+function endGame(){
+    endGameText.innerHTML = 'YOU<br>WON';
+    endGameScreen.classList.add('win');
+    endGameScreen.classList.remove('hidden');
 }
