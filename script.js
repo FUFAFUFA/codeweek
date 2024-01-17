e4fa9be9920190fa161901f1ab7af077c54803e4
Herman <58345489+FUFAFUFA@users.noreply.github.com>
2024-01-17 20:12:28 +0100

    js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // const (constant, never changes)
-
 // let (let me change)
 
 // WEB-API
-const scoreCounter = document.querySelector('core-counter');
 const grid = document.querySelector('.grid');
+const scoreCounter = document.querySelector('.score-counter');
+const endGameScreen = document.querySelector('.end-game-screen');
+const endGameText = document.querySelector('.end-game-text');
+const playAgainButton = document.querySelector('.play-again');
+
+// GAME
+const totalCells = 100;
+const totalBombs = 90;
+const maxScore = 5;
+const bomsList = [];
+
+let score = 0;
+
+const cell = document.createElement('div');
+cell.classList.add('cell');
+
+for (let i = 1; i <= 100; i++) {
+cell.classList.add(cell);
+grid.appendChild(cell);
+}
